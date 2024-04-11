18e2a965f05598a18eaf187eb52260619da86b08
Sathvik79 <sathvikjain500@gmail.com>
2024-04-11 14:48:31 +0530

    link css

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
 
 var items = [];
 
